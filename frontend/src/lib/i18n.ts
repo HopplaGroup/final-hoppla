a9ad65d5cdf9d861440a4cb05b2c5da5d3d4ac1e
mishodzuliashvili <mishodzuliashvili1@gmail.com>
2024-08-11 00:57:05 +0400

    add default language

--- frontend/src/lib/i18n.ts
+++ frontend/src/lib/i18n.ts
@@ -1,9 +1,10 @@
-import { Navigation, Middleware, PrefixStrategy } from "@inlang/paraglide-next"
-import { AvailableLanguageTag } from "@/paraglide/runtime"
+import { Navigation, Middleware, PrefixStrategy } from "@inlang/paraglide-next";
+import { AvailableLanguageTag } from "@/paraglide/runtime";
 
 export const strategy = PrefixStrategy<AvailableLanguageTag>({
-	prefixDefault: "always"
-})
+  prefixDefault: "always",
+});
 
-export const middleware = Middleware({ strategy })
-export const { Link, useRouter, usePathname, redirect, permanentRedirect } = Navigation({ strategy })
+export const middleware = Middleware({ strategy });
+export const { Link, useRouter, usePathname, redirect, permanentRedirect } =
+  Navigation({ strategy });
