a4982043a313baaf5db902b36a8d1f4588671e9f
Demetre Shonia <shonka.demeloper@gmail.com>
2024-09-24 17:29:18 +0400

    Refactor bookRide function to use updated public key for payment verification

--- frontend/src/lib/bog/book-ride.ts
+++ frontend/src/lib/bog/book-ride.ts
@@ -3,40 +3,38 @@ import { redirect } from "next/navigation";
 import requestOrder from "./request-order";
 import { getUser } from "../utils/auth";
 
-const FIXED_PRICE = 0.0; // 0.1 = 10 tetri
+const FIXED_PRICE = 0.2; // 0.1 = 10 tetri
 
 const CALLBACK_URL = "https://hoppla.ge/api/bog/callback";
 
 export default async function bookRide(rideId: string) {
-    const user = await getUser();
-    if (!user) {
-        return {
-            success: false,
-        };
-    }
-
-    const external_order_id = user.id + "_" + rideId;
-
-    const orderRequest = await requestOrder({
-        callback_url: CALLBACK_URL,
-        external_order_id,
-        redirect_urls: {
-            success: "https://hoppla.ge/rides/" + rideId + "?success=true",
-            fail: "https://hoppla.ge/rides/" + rideId + "?success=false",
-        },
-        purchase_units: {
-            total_amount: FIXED_PRICE,
-            basket: [
-                { product_id: rideId, quantity: 1, unit_price: FIXED_PRICE },
-            ],
-        },
-    });
-
-    if (orderRequest.success) {
-        redirect(orderRequest._links.redirect.href);
-    }
-
+  const user = await getUser();
+  if (!user) {
     return {
-        success: false,
+      success: false,
     };
+  }
+
+  const external_order_id = user.id + "_" + rideId;
+
+  const orderRequest = await requestOrder({
+    callback_url: CALLBACK_URL,
+    external_order_id,
+    redirect_urls: {
+      success: "https://hoppla.ge/rides/" + rideId + "?success=true",
+      fail: "https://hoppla.ge/rides/" + rideId + "?success=false",
+    },
+    purchase_units: {
+      total_amount: FIXED_PRICE,
+      basket: [{ product_id: rideId, quantity: 1, unit_price: FIXED_PRICE }],
+    },
+  });
+
+  if (orderRequest.success) {
+    redirect(orderRequest._links.redirect.href);
+  }
+
+  return {
+    success: false,
+  };
 }
--- frontend/src/lib/bog/refund-payment.ts
+++ frontend/src/lib/bog/refund-payment.ts
@@ -3,53 +3,57 @@ import "server-only";
 import getToken from "./get-token";
 
 type RefundResponse = {
-    key: string;
-    message: string;
-    action_id: string;
-    success: true;
+  key: string;
+  message: string;
+  action_id: string;
+  success: true;
 };
 
 type ErrorResponse = {
-    success: false;
+  success: false;
 };
 
 export default async function refundPayment(
-    orderId: string,
-    amount?: number
+  orderId: string,
+  amount?: number
 ): Promise<RefundResponse | ErrorResponse> {
-    const refundUrl = `https://api.bog.ge/payments/v1/payment/refund/${orderId}`;
+  const refundUrl = `https://api.bog.ge/payments/v1/payment/refund/${orderId}`;
 
-    try {
-        const tokenResponse = await getToken();
-        if (!tokenResponse.success) {
-            return {
-                success: false,
-            };
-        }
-        const response = await fetch(refundUrl, {
-            method: "POST",
-            headers: {
-                "Accept-Language": "ka",
-                Authorization: `Bearer ${tokenResponse.access_token}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ ...(amount && { amount }) }),
-        });
-
-        if (!response.ok) {
-            return {
-                success: false,
-            };
-        }
-
-        const data = await response.json();
-        return {
-            ...data,
-            success: true,
-        };
-    } catch (error) {
-        return {
-            success: false,
-        };
+  try {
+    const tokenResponse = await getToken();
+    if (!tokenResponse.success) {
+      return {
+        success: false,
+      };
     }
+    const response = await fetch(refundUrl, {
+      method: "POST",
+      headers: {
+        "Accept-Language": "ka",
+        Authorization: `Bearer ${tokenResponse.access_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...(amount && { amount }),
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Refund payment failed:", errorData);
+      return {
+        success: false,
+      };
+    }
+
+    const data = await response.json();
+    return {
+      ...data,
+      success: true,
+    };
+  } catch (error) {
+    return {
+      success: false,
+    };
+  }
 }
