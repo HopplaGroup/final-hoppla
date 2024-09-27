c3ba3d885a04b8d1ea8ddfa5f52c795b98833520
Misho Dzuliashvili <mishodzuliashvili1@gmail.com>
2024-09-27 19:39:13 +0400

    Refactor constants file to update refund and ride prices

--- frontend/src/lib/bog/book-ride.ts
+++ frontend/src/lib/bog/book-ride.ts
@@ -2,39 +2,40 @@
 import { redirect } from "next/navigation";
 import requestOrder from "./request-order";
 import { getUser } from "../utils/auth";
-
-const FIXED_PRICE = 0.2; // 0.1 = 10 tetri
+import { RIDE_PRICE } from "./constants";
 
 const CALLBACK_URL = "https://hoppla.ge/api/bog/callback";
 
 export default async function bookRide(rideId: string) {
-  const user = await getUser();
-  if (!user) {
+    const user = await getUser();
+    if (!user) {
+        return {
+            success: false,
+        };
+    }
+
+    const external_order_id = user.id + "_" + rideId;
+
+    const orderRequest = await requestOrder({
+        callback_url: CALLBACK_URL,
+        external_order_id,
+        redirect_urls: {
+            success: "https://hoppla.ge/rides/" + rideId + "?success=true",
+            fail: "https://hoppla.ge/rides/" + rideId + "?success=false",
+        },
+        purchase_units: {
+            total_amount: RIDE_PRICE,
+            basket: [
+                { product_id: rideId, quantity: 1, unit_price: RIDE_PRICE },
+            ],
+        },
+    });
+
+    if (orderRequest.success) {
+        redirect(orderRequest._links.redirect.href);
+    }
+
     return {
-      success: false,
+        success: false,
     };
-  }
-
-  const external_order_id = user.id + "_" + rideId;
-
-  const orderRequest = await requestOrder({
-    callback_url: CALLBACK_URL,
-    external_order_id,
-    redirect_urls: {
-      success: "https://hoppla.ge/rides/" + rideId + "?success=true",
-      fail: "https://hoppla.ge/rides/" + rideId + "?success=false",
-    },
-    purchase_units: {
-      total_amount: FIXED_PRICE,
-      basket: [{ product_id: rideId, quantity: 1, unit_price: FIXED_PRICE }],
-    },
-  });
-
-  if (orderRequest.success) {
-    redirect(orderRequest._links.redirect.href);
-  }
-
-  return {
-    success: false,
-  };
 }
--- frontend/src/lib/bog/constants.ts
+++ frontend/src/lib/bog/constants.ts
@@ -1,5 +1,2 @@
-export const REFUND_PRICE = 0.1;
-export const RIDE_PRICE = 0.5;
-
-
-
+export const REFUND_PRICE = 0.01;
+export const RIDE_PRICE = 0.02;
