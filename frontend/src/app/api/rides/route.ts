285c5fcba8c99706bcca0f812442ddbed8e68a2d
Demetre Shonia <shonka.demeloper@gmail.com>
2024-10-01 19:01:40 +0400

    Fix filling of passengers on a ride

--- frontend/src/app/api/rides/route.ts
+++ frontend/src/app/api/rides/route.ts
@@ -1,73 +1,71 @@
 import PLACES from "@/lib/constants/places";
 import {
-    defaultSortBy,
-    validSortFields,
-    validSortOrders,
+  defaultSortBy,
+  validSortFields,
+  validSortOrders,
 } from "@/lib/constants/search";
 import db from "@/lib/utils/db";
 import { Prisma } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const searchParams = request.nextUrl.searchParams;
-    const from = searchParams.get("from");
-    const to = searchParams.get("to");
-    const page = parseInt(searchParams.get("page") || "1", 10);
-    const pageSize = parseInt(searchParams.get("pageSize") || "10", 10);
-    const sortBy = searchParams.get("sortBy");
-    const departure = searchParams.get("departure");
-    const availableSeats = searchParams.get("availableSeats");
-    const rulesTmp = searchParams.get("rules");
-    const rules = rulesTmp ? rulesTmp.split(",") : [];
+  const searchParams = request.nextUrl.searchParams;
+  const from = searchParams.get("from");
+  const to = searchParams.get("to");
+  const page = parseInt(searchParams.get("page") || "1", 10);
+  const pageSize = parseInt(searchParams.get("pageSize") || "10", 10);
+  const sortBy = searchParams.get("sortBy");
+  const departure = searchParams.get("departure");
+  const availableSeats = searchParams.get("availableSeats");
+  const rulesTmp = searchParams.get("rules");
+  const rules = rulesTmp ? rulesTmp.split(",") : [];
 
-    const [sortField, sortOrder] = (sortBy || defaultSortBy).split(":");
+  const [sortField, sortOrder] = (sortBy || defaultSortBy).split(":");
 
-    const orderByField = validSortFields.includes(sortField)
-        ? sortField
-        : "driverAverageRating";
-    const orderByOrder = validSortOrders.includes(sortOrder)
-        ? sortOrder
-        : "desc";
+  const orderByField = validSortFields.includes(sortField)
+    ? sortField
+    : "driverAverageRating";
+  const orderByOrder = validSortOrders.includes(sortOrder) ? sortOrder : "desc";
 
-    let orderByClause = Prisma.sql`COALESCE(AVG(review."rating"), 0) DESC`;
+  let orderByClause = Prisma.sql`COALESCE(AVG(review."rating"), 0) DESC`;
 
-    switch (orderByField) {
-        case "price":
-            if (orderByOrder === "asc") {
-                orderByClause = Prisma.sql`"price" ASC`;
-            } else {
-                orderByClause = Prisma.sql`"price" DESC`;
-            }
-            break;
-        case "driverAverageRating":
-            if (orderByOrder === "asc") {
-                orderByClause = Prisma.sql`COALESCE(AVG(review."rating"), 0) ASC`;
-            } else {
-                orderByClause = Prisma.sql`COALESCE(AVG(review."rating"), 0) DESC`;
-            }
-            break;
-        case "departure":
-            if (orderByOrder === "asc") {
-                orderByClause = Prisma.sql`"departure" ASC`;
-            } else {
-                orderByClause = Prisma.sql`"departure" DESC`;
-            }
-            break;
-    }
+  switch (orderByField) {
+    case "price":
+      if (orderByOrder === "asc") {
+        orderByClause = Prisma.sql`"price" ASC`;
+      } else {
+        orderByClause = Prisma.sql`"price" DESC`;
+      }
+      break;
+    case "driverAverageRating":
+      if (orderByOrder === "asc") {
+        orderByClause = Prisma.sql`COALESCE(AVG(review."rating"), 0) ASC`;
+      } else {
+        orderByClause = Prisma.sql`COALESCE(AVG(review."rating"), 0) DESC`;
+      }
+      break;
+    case "departure":
+      if (orderByOrder === "asc") {
+        orderByClause = Prisma.sql`"departure" ASC`;
+      } else {
+        orderByClause = Prisma.sql`"departure" DESC`;
+      }
+      break;
+  }
 
-    let rulesCondition = Prisma.sql``;
+  let rulesCondition = Prisma.sql``;
 
-    if (rules.length > 0) {
-        rulesCondition = Prisma.sql`AND (
+  if (rules.length > 0) {
+    rulesCondition = Prisma.sql`AND (
       SELECT COUNT(*)
       FROM "RideRule" ridetorule
       WHERE ridetorule."rideId" = ride.id
         AND ridetorule."ruleId" IN (${Prisma.join(rules)})
     ) = ${rules.length}`;
-    }
+  }
 
-    try {
-        const whereClause = Prisma.sql`
+  try {
+    const whereClause = Prisma.sql`
        WHERE (${from}::text IS NULL OR ride."from" = ${from})
             AND (${to}::text IS NULL OR ride."to" = ${to})
             AND (${departure}::text IS NULL OR (
@@ -79,9 +77,9 @@ export async function GET(request: NextRequest) {
             AND ride."departure" > NOW()
             ${rulesCondition}
             `;
-        // TODO: will this work NOW()
+    // TODO: will this work NOW()
 
-        const totalCount = await db.$queryRaw`
+    const totalCount = await db.$queryRaw`
           SELECT COUNT(*) as count
             FROM (
                 SELECT ride.id
@@ -99,9 +97,9 @@ export async function GET(request: NextRequest) {
                 ))
             ) AS filtered_rides
            `;
-        //    TODO: and avaialble seats that remaining should be more than zero
+    //    TODO: and avaialble seats that remaining should be more than zero
 
-        const rides = await db.$queryRaw`
+    const rides = await db.$queryRaw`
           SELECT ride.id as id,
           ride."availableSeats",
                 ride."price",
@@ -132,7 +130,7 @@ export async function GET(request: NextRequest) {
                  'id', passenger.id,
                  'profileImg', passenger."profileImg",
                  'name', passenger."name"
-               )) FILTER (WHERE passenger.id IS NOT NULL),
+               )) FILTER (WHERE ridepassengerrequest.status = 'ACCEPTED' AND passenger.id IS NOT NULL),
                '[]'::json
              ) AS "passengers"
           FROM "Ride" ride
@@ -156,20 +154,20 @@ export async function GET(request: NextRequest) {
           LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize};
   `;
 
-        // @ts-ignore
-        // console.log(rides);
-        return NextResponse.json({
-            rides: (rides as any).map((ride: any) => ({
-                ...ride,
-                departure: new Date(ride.departure),
-            })),
-            totalCount: (totalCount as any)[0].count.toString(),
-        });
-    } catch (error) {
-        // console.error(error);
-        return NextResponse.json(
-            { error: "Error fetching rides" },
-            { status: 500 }
-        );
-    }
+    // @ts-ignore
+    // console.log(rides);
+    return NextResponse.json({
+      rides: (rides as any).map((ride: any) => ({
+        ...ride,
+        departure: new Date(ride.departure),
+      })),
+      totalCount: (totalCount as any)[0].count.toString(),
+    });
+  } catch (error) {
+    // console.error(error);
+    return NextResponse.json(
+      { error: "Error fetching rides" },
+      { status: 500 }
+    );
+  }
 }
