999cd60d0e6b78b6620798cb9e88f8177cda72cd
Demetre Shonia <shonka.demeloper@gmail.com>
2024-10-02 16:52:07 +0400

    Add sign out button in profile

--- frontend/src/app/(not-new-user)/(with-navbar-and-footer)/profile/settings/page.tsx
+++ frontend/src/app/(not-new-user)/(with-navbar-and-footer)/profile/settings/page.tsx
@@ -1,5 +1,9 @@
 import { getUser } from "@/lib/utils/auth";
 import { ProfileSettingsForm } from "./profile-settings-form";
+import { Button } from "@react-email/components";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import * as m from "@/paraglide/messages.js";
+import { Separator } from "@/components/ui/separator";
 
 export default async function ProfileSettingsPage() {
   const user = await getUser();
@@ -17,6 +21,12 @@ export default async function ProfileSettingsPage() {
 
         <div className="mt-4">
           <ProfileSettingsForm user={user as any} />
+          <Separator />
+          <Button type="button" className="mt-4  rounded-lg bg-gray-500">
+            <LogoutLink className="flex items-center py-2 px-5 text-white justify-center">
+              {m.extra_lucky_rook_trust()}
+            </LogoutLink>
+          </Button>
         </div>
       </div>
     </main>
