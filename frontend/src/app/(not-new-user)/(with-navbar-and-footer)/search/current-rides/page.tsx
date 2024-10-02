af95cbac234687f549ccf3acf45fb160eb25bd1a
Demetre Shonia <shonka.demeloper@gmail.com>
2024-10-02 19:16:51 +0400

    Add Current Ride Logic

--- frontend/src/app/(not-new-user)/(with-navbar-and-footer)/search/current-rides/page.tsx
+++ frontend/src/app/(not-new-user)/(with-navbar-and-footer)/search/current-rides/page.tsx
@@ -1,7 +1,10 @@
+"use client";
 import React from "react";
 import RideCard from "../_components/ride-card";
 import { RideResponse } from "../response-ride-type";
 import { Separator } from "@/components/ui/separator";
+import { useFindManyRide } from "@/lib/hooks";
+import { useUser } from "@/lib/providers/user-provider";
 
 const fakeRide: RideResponse = {
   id: "ride123",
@@ -47,22 +50,106 @@ const fakeRide: RideResponse = {
 };
 
 const CurrentRides = () => {
-  const currentRides: RideResponse[] = [fakeRide];
+  // const currentRides: RideResponse[] = [fakeRide];
+
+  const { user } = useUser();
+  const { data: currentRides, isLoading } = useFindManyRide(
+    {
+      where: {
+        OR: [
+          {
+            driverId: user?.id,
+          },
+          {
+            ridePassengerRequests: {
+              some: {
+                passengerId: user?.id,
+                status: {
+                  in: ["ACCEPTED", "PENDING"],
+                },
+              },
+            },
+          },
+        ],
+      },
+      include: {
+        car: true,
+        driver: true,
+        ridePassengerRequests: {
+          include: {
+            passenger: true,
+          },
+        },
+        rideRules: {
+          include: {
+            rule: true,
+          },
+        },
+      },
+    },
+    {
+      enabled: !!user,
+    }
+  );
+  const mappedCurrentRides = currentRides?.map(
+    (ride) =>
+      ({
+        availableSeats: ride.availableSeats,
+        departure: new Date(ride.departure),
+        distance: ride.distance,
+        to: ride.to,
+        duration: ride.duration,
+        from: ride.from,
+        id: ride.id,
+        price: String(ride.price),
+        car: {
+          id: ride.car.id,
+          type: ride.car.type,
+        },
+        driver: {
+          id: ride.driver.id,
+          name: ride.driver.name,
+          profileImg: ride.driver.profileImg,
+          averageRating: 0,
+        },
+        passengers: ride.ridePassengerRequests.map((passenger) => ({
+          id: passenger.passenger.id,
+          name: passenger.passenger.name,
+          profileImg: passenger.passenger.profileImg,
+        })),
+        rules: ride.rideRules.map((rule) => ({
+          id: rule.id,
+          description: rule.rule.description,
+        })),
+      } as RideResponse)
+  );
   return (
     <div className="container">
       <div className="text-2xl text-center mt-4">Your current rides</div>
       <Separator className="mt-4 mb-8" />
 
-      {currentRides && currentRides.length > 0 ? (
-        <div>
-          {currentRides.map((ride) => (
-            <RideCard key={ride.id} ride={ride} />
-          ))}
+      {mappedCurrentRides && mappedCurrentRides.length > 0 ? (
+        <div className="space-y-4">
+          {mappedCurrentRides
+            .toSorted((a, b) => {
+              if (a.driver.id === user?.id) return -1;
+              if (b.driver.id === user?.id) return 1;
+              return 0; // Keep others in same order
+            })
+            .map((ride) => (
+              <RideCard key={ride.id} ride={ride} />
+            ))}
         </div>
       ) : (
-        <div className="text-center text-gray-500">
-          You have not booked any rides yet.
-        </div>
+        <>
+          {isLoading ? (
+            <div className="text-center text-gray-500">Loading...</div>
+          ) : (
+            <div className="text-center text-gray-500">
+              You have not booked any rides yet.
+            </div>
+          )}
+        </>
       )}
     </div>
   );
