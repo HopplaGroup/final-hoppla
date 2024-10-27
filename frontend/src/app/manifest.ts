14ddfe823dcc430bac00468e27018c12cbdd84e0
Misho Dzuliashvili <mishodzuliashvili1@gmail.com>
2024-10-27 18:20:46 +0400

    return old manifest

--- frontend/src/app/manifest.ts
+++ frontend/src/app/manifest.ts
@@ -1,27 +1,25 @@
 import type { MetadataRoute } from "next";
-
 export default function manifest(): MetadataRoute.Manifest {
     return {
         name: "Hoppla",
         short_name: "Hoppla",
         description: "Ride sharing app",
-        id: "/",
         start_url: "/",
-        screenshots: [
-            {
-                src: "/web-app-manifest-512x512.png",
-                sizes: "512x512",
-                type: "image/png",
-            },
-        ],
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#ffffff",
         icons: [
             {
-                src: "/favicon.ico",
-                sizes: "any",
-                type: "image/x-icon",
+                src: "/web-app-manifest-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "maskable",
+            },
+            {
+                src: "/web-app-manifest-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "maskable",
             },
         ],
     };
