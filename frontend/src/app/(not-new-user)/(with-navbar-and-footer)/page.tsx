1d35b8aa33aa0eb1bbce831afbc2b2667e13e840
Demetre Shonia <shonka.demeloper@gmail.com>
2025-04-15 17:24:09 +0400

    Add admin page link and fix visual defects on mobile

--- frontend/src/app/(not-new-user)/(with-navbar-and-footer)/page.tsx
+++ frontend/src/app/(not-new-user)/(with-navbar-and-footer)/page.tsx
@@ -13,7 +13,6 @@ import { Hero } from "./Hero";
 export default function Home() {
   return (
     <>
-      <div className="mt-20"></div>
       <Hero backgroundImage={highWay} />
 
       {/* <Features /> */}
